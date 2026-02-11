d36c550121103c951d152f510ecb8696a1b47eb8
JhonnyEkizde <jhonnycarrizo2005@gmail.com>
2026-02-10 22:44:09 -0400

    Add files via upload

---
+++ Login Proyecto V3/procesar.php
@@ -0,0 +1,3 @@
+Inicio de sesion exitoso xd
+<br>
+<a href="index.php">Volver</a>
---
+++ Login Proyecto V3/registro.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+    <h1>error 404</h1>
+    <h2>Me dio flojera hacer el registro xd</h2>
+</body>
+</html>
